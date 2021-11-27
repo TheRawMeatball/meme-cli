ec7c3d9e709d935ddea24790a4fec0a4dc8758f0
TheRawMeatball <therawmeatball@gmail.com>
2021-11-27 17:13:54 +0300

    remove debug changes

--- meme-cli/src/image_io.rs
+++ meme-cli/src/image_io.rs
@@ -1,9 +1,9 @@
-// #[cfg(not(target_os = "android"))]
-// mod arboard;
-// #[cfg(not(target_os = "android"))]
-// pub use self::arboard::*;
+#[cfg(not(target_os = "android"))]
+mod arboard;
+#[cfg(not(target_os = "android"))]
+pub use self::arboard::*;
 
-// #[cfg(target_os = "android")]
+#[cfg(target_os = "android")]
 mod termux;
-// #[cfg(target_os = "android")]
+#[cfg(target_os = "android")]
 pub use termux::*;
